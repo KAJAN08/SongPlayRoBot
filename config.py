757b5771e5e7d5af8132eafdeb932743375632a4
KAJAN08 <92639970+KAJAN08@users.noreply.github.com>
2023-01-29 00:35:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 API_ID = int(os.getenv("15807442"))
 API_HASH = os.getenv("c993142b7c8b44e8efb1093c2321071d")
 BOT_TOKEN = os.getenv("5876469070:AAHittpmqH6UAc8g7DNzOBQAd2nTG9bRAsk")
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("https://github.com/KAJAN08/SongPlayRoBot.git")
 OWNER_ID = list({int(x) for x in os.environ.get("1699462699", "").split()})
